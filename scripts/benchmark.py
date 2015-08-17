363a9f61f7ce7a768414874d6c2e3ee53cae67db
Matthew Perry <perrygeo@gmail.com>
2015-08-17 10:57:22 -0400

    test various methods in benchmarks

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,13 +1,18 @@
 import pandas
+import math
+import os
+import rasterio
 from rasterstats import zonal_stats
+from statistics import median  # python 3.4 only
+from subprocess import check_output
 
-
-def query_clim(df, rcp, lat, lng, variable="tx", period="70"):
+def query_clim(df, rcp, lat, lng, variable="tx", period="70", units="C"):
     sub = df[(df['variable'] == variable) &
              (df['rcp'] == rcp) &
              (df['period'] == period)]
 
     mins = []
+    medians = []
     maxes = []
     wkt = 'POINT({} {})'.format(lng, lat)
     for month in [x+1 for x in range(12)]:
@@ -16,20 +21,134 @@ def query_clim(df, rcp, lat, lng, variable="tx", period="70"):
         # Loop through paths and run zonal stats against each
         for path in monthsub['path']:
             val = zonal_stats(wkt, path, stats="mean")[0]['mean']
+            if math.isnan(val):
+                raise ValueError("No data for {} at this location".format(path))
             if variable in ('tx', 'tn'):
-                val = val / 10.0  # Temp are mult by 10x C, divided by 10 to get C
+                # Temp are mult by 10x C, divided by 10 to get C
+                val = val / 10.0
+                # Then convert to Fahrenheit if specified
+                if units == "F":
+                    val = ((val / 5.0) * 9) + 32
+
             vals.append(val)
 
         # find min, max for this month
         mins.append(min(vals))
         maxes.append(max(vals))
+        medians.append(median(vals))
 
-    # months = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec".split()
-    # from collections import OrderedDict
-    # return OrderedDict(zip(months, zip(mins, maxes)))
+    data = {
+        "period": period,
+        "min": min(mins),
+        "max": max(maxes),
+        "data": [{'month': i+1, 'min': x[0], 'median': x[1], 'max': x[2]}
+                 for i, x in enumerate(zip(mins, medians, maxes))]}
 
-    return [{'month': i+1, 'min': x[0], 'max': x[1]}
-            for i, x in enumerate(zip(mins, maxes))]
+    return data
+
+
+def tiny_window(dataset, x, y):
+    r, c = dataset.index(x, y)
+    return ((r, r+1), (c, c+1))
+
+
+def make_vrt(paths, variable, rcp, period, month):
+    vrtpath = "/tmp/{}_{}_{}_{}.vrt".format(variable, rcp, period, month)
+    if not os.path.exists(vrtpath):
+        vargs = ["gdalbuildvrt", "-separate", vrtpath]
+        vargs.extend(paths)
+        check_output(vargs)
+    return vrtpath
+
+
+def query_clim2(df, rcp, lat, lng, variable="tx", period="70", units="C"):
+    sub = df[(df['variable'] == variable) &
+             (df['rcp'] == rcp) &
+             (df['period'] == period)]
+
+    mins = []
+    medians = []
+    maxes = []
+    for month in [x+1 for x in range(12)]:
+        monthsub = sub[df['month'] == month]
+        vals = []
+        # Loop through paths and run zonal stats against each
+        for path in monthsub['path']:
+            with rasterio.open(path) as src:
+                win = tiny_window(src, lng, lat)
+                arr = src.read(window=win)
+
+            val = arr[0][0][0]
+            # val = zonal_stats(wkt, path, stats="mean")[0]['mean']
+            if math.isnan(val):
+                raise ValueError("No data for {} at this location".format(path))
+            if variable in ('tx', 'tn'):
+                # Temp are mult by 10x C, divided by 10 to get C
+                val = val / 10.0
+                # Then convert to Fahrenheit if specified
+                if units == "F":
+                    val = ((val / 5.0) * 9) + 32
+
+            vals.append(val)
+
+        # find min, max for this month
+        mins.append(min(vals))
+        maxes.append(max(vals))
+        medians.append(median(vals))
+
+    data = {
+        "period": period,
+        "min": min(mins),
+        "max": max(maxes),
+        "data": [{'month': i+1, 'min': x[0], 'median': x[1], 'max': x[2]}
+                 for i, x in enumerate(zip(mins, medians, maxes))]}
+
+    return data
+
+
+def query_clim3(df, rcp, lat, lng, variable="tx", period="70", units="C"):
+    sub = df[(df['variable'] == variable) &
+             (df['rcp'] == rcp) &
+             (df['period'] == period)]
+
+    mins = []
+    medians = []
+    maxes = []
+    for month in [x+1 for x in range(12)]:
+        monthsub = sub[df['month'] == month]
+        # Loop through paths and run zonal stats against each
+        vrt = make_vrt([path for path in monthsub['path']],
+                       variable, rcp, period, month)
+        with rasterio.open(vrt) as src:
+            win = tiny_window(src, lng, lat)
+            arr = src.read(window=win)
+
+        vals = arr[:, 0, 0]
+
+        # TODO null checks
+        # if math.isnan(val):
+        #     raise ValueError("No data for {} at this location".format(path))
+
+        if variable in ('tx', 'tn'):
+            # Temp are mult by 10x C, divided by 10 to get C
+            vals = vals / 10.0
+            # Then convert to Fahrenheit if specified
+            if units == "F":
+                vals = ((vals / 5.0) * 9) + 32
+
+        # find min, max for this month
+        mins.append(vals.min())
+        maxes.append(vals.max())
+        medians.append(median(vals))
+
+    data = {
+        "period": period,
+        "min": min(mins),
+        "max": max(maxes),
+        "data": [{'month': i+1, 'min': x[0], 'median': x[1], 'max': x[2]}
+                 for i, x in enumerate(zip(mins, medians, maxes))]}
+
+    return data
 
 
 # Load dataframe in global scope
@@ -37,14 +156,39 @@ df = pandas.read_csv("climate_data.csv")
 
 
 if __name__ == "__main__":
-    lat = 21.94
-    lng = 11.25
-    data = [
-        {"period": "mid", "data": query_clim(df, lat=lat, lng=lng, rcp='na', period='mid')},
-        {"period": "lgm", "data": query_clim(df, lat=lat, lng=lng, rcp='na', period='lgm')},
-        {"period": "cur", "data": query_clim(df, lat=lat, lng=lng, rcp='na', period='current')},
-        {"period": "y50-26", "data": query_clim(df, lat=lat, lng=lng, rcp='26', period='50')},
-        {"period": "y70-26", "data": query_clim(df, lat=lat, lng=lng, rcp='26', period='70')},
-        {"period": "y50-85", "data": query_clim(df, lat=lat, lng=lng, rcp='85', period='50')},
-        {"period": "y70-85", "data": query_clim(df, lat=lat, lng=lng, rcp='85', period='70')}
-    ]
+    import timeit
+
+    method1 = query_clim(df, lat=12.05, lng=-61.75, units="C", period="70", variable="tx", rcp="85")
+    method2 = query_clim2(df, lat=12.05, lng=-61.75, units="C", period="70", variable="tx", rcp="85")
+    method3 = query_clim3(df, lat=12.05, lng=-61.75, units="C", period="70", variable="tx", rcp="85")
+    assert method1 == method2
+
+    cmd = """query_clim(df,
+                      lat=12.05,
+                      lng=-61.75,
+                      units="C",
+                      period="70",
+                      variable="tx",
+                      rcp="85")"""
+
+    print(timeit.timeit(cmd, number=3, setup="from __main__ import df, query_clim"))
+
+    cmd2 = """query_clim2(df,
+                      lat=12.05,
+                      lng=-61.75,
+                      units="C",
+                      period="70",
+                      variable="tx",
+                      rcp="85")"""
+
+    print(timeit.timeit(cmd2, number=3, setup="from __main__ import df, query_clim2"))
+
+    cmd3 = """query_clim3(df,
+                      lat=12.05,
+                      lng=-61.75,
+                      units="C",
+                      period="70",
+                      variable="tx",
+                      rcp="85")"""
+
+    print(timeit.timeit(cmd3, number=3, setup="from __main__ import df, query_clim3"))
